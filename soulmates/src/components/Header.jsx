f88e6ff3587d4b5d9693ef759b6864bed42a0253
Stancz Boglár Anna <sbanna0422@gmail.com>
2025-10-30 15:01:56 +0100

    fix(header):navlink renaming and fix css

--- soulmates/src/components/Header.jsx
+++ soulmates/src/components/Header.jsx
@@ -3,23 +3,33 @@ import { Link, NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <header className="flex gap-4 p-4 bg-gray-100 shadow-md navbar">
-      <div className="logo">✨ My Energy App</div>
+      <div className="logo">✨ SoulmMates ✨</div>
       <ul className="flex gap-4">
-        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
-          Home
-        </NavLink>
-        <NavLink to="/reading" className={({ isActive }) => (isActive ? "active" : "")}>
-          Reading
-        </NavLink>
-        <NavLink to="/moodboard" className={({ isActive }) => (isActive ? "active" : "")}>
-          Daily Mood
-        </NavLink>
-        <NavLink to="/profile" className={({ isActive }) => (isActive ? "active" : "")}>
-          Profile
-        </NavLink>
-        <NavLink to="/friends" className={({ isActive }) => (isActive ? "active" : "")}>
-          Friends
-        </NavLink>
+        <li>
+          <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+            Home
+          </NavLink>
+        </li>
+       <li>
+          <NavLink to="/reading" className={({ isActive }) => (isActive ? "active" : "")}>
+            Reading
+          </NavLink>
+       </li>
+       <li>
+          <NavLink to="/moodboard" className={({ isActive }) => (isActive ? "active" : "")}>
+            Daily Mood
+          </NavLink>
+       </li>
+        <li>
+          <NavLink to="/profile" className={({ isActive }) => (isActive ? "active" : "")}>
+            Profile
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/friends" className={({ isActive }) => (isActive ? "active" : "")}>
+            Friends
+          </NavLink>
+        </li>
       </ul>
     </header>
   );
--- soulmates/src/pages/Home.jsx
+++ soulmates/src/pages/Home.jsx
@@ -1,9 +1,6 @@
-import Header from '../components/Header';
-
 const Home = () => {
   return (
     <div>
-      <Header />
       <h1>Today’s Energies Are Speaking — Are You Listening?</h1>
     </div>
   )
