215fa0a33992c543c7b0f77e8ac17a1f36eda28b
Bobo-013 <somogyiboglarka08@gmail.com>
2025-12-06 12:07:50 +0100

    fix: Fix saving moods

--- soulmates/src/components/MoodGraph.jsx
+++ soulmates/src/components/MoodGraph.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect } from 'react'
 import { useLocation, useNavigate } from "react-router-dom";
 import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from "recharts";
 import "../assets/moodboard.css";
@@ -6,8 +6,25 @@ import "../assets/moodboard.css";
 function MoodGraph(){
     const location = useLocation();
     const navigate = useNavigate();
+    const [data, setData] = useState([]);
 
-    const data = location.state?.data || [];
+  useEffect(() => {
+    const loadMoods = async () => {
+      if (location.state?.data) {
+        setData(location.state.data);
+      } else {
+        try {
+          const result = await window.storage.get('moods');
+          if (result?.value) {
+            setData(JSON.parse(result.value));
+          }
+        } catch (error) {
+          console.log('No moods found');
+        }
+      }
+    };
+    loadMoods();
+  }, [location.state]);
 
     const transformed = data.map((m, i) =>
     ({
